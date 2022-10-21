974a0cc25d167ec4906d251dd089e2bbb5067389
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-10-21 18:09:33 +0800

    update javascript version of compress-simple

--- compress-simple/compress.js
+++ compress-simple/compress.js
@@ -1,36 +1,33 @@
-let input = [0,1, 1, 1, 2, 2, 3, 4, 5, 5, 1, 1];
-let answer = [0,[3, 1], [2, 2], 3, 4, [2, 5], [2, 1]];
+let input = [0, 1, 1, 1, 2, 2, 3, 4, 5, 5, 1, 1]
+let answer = [0, [3, 1], [2, 2], 3, 4, [2, 5], [2, 1]]
 
 function compress(input) {
-    let res = [];
-    let isFirst = true;
-    let last;
-    let current;
-    let count = 0;
-    for (let i = 0; i < input.length; i++) {
-        current = input[i];
-        if (current == last || isFirst) {
-            count++;
-        } else {
-            if (count == 0) {
-                res.push(current);
-            } else {
-                res.push([count, last]);
-                count = 0;
-            }
-        }
-        isFirst = false;
-        last = current;
-    }
-    if (count == 0) {
-        res.push(current);
+  if (input.length == 0) {
+    return input
+  }
+  let res = []
+  let buffer = [1, input[0]]
+  for (let i = 1; i < input.length; i++) {
+    let value = input[i]
+    if (value == buffer[1]) {
+      buffer[0]++
     } else {
-        res.push([count, current]);
-        count = 0;
+      if (buffer[0] == 1) {
+        res.push(buffer[1])
+      } else {
+        res.push(buffer)
+      }
+      buffer = [1, value]
     }
-    return res;
+  }
+  if (buffer[0] == 1) {
+    res.push(buffer[1])
+  } else {
+    res.push(buffer)
+  }
+  return res
 }
 
-console.log('input', input);
-console.log('answer', answer);
-console.log('output', compress(input));
+console.log('input', input)
+console.log('answer', answer)
+console.log('output', compress(input))
